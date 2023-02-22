9129b8f9c8e1ed427685abf742b681641c5b169d
TuDo1403 <tudo.dev@gmail.com>
2023-02-23 00:46:36 +0700

    feat: update taxable

---
+++ contracts/internal/interfaces/ITaxable.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+interface ITaxable {
+    error Taxable__TaxDisabled();
+    error Taxable__AlreadyEnabled();
+    error Taxable__InvalidArguments();
+
+    event TaxEnabled(
+        address indexed operator,
+        uint256 indexed start,
+        uint256 indexed stop
+    );
+
+    event TaxBeneficiarySet(
+        address indexed operator,
+        address indexed oldBeneficiary,
+        address indexed newBeneficiary
+    );
+
+    function toggleTax() external;
+
+    function setTaxBeneficiary(address taxBeneficiary_) external;
+
+    function tax(
+        address token_,
+        uint256 amount_
+    ) external pure returns (uint256);
+
+    function taxFraction(address token_) external pure returns (uint256);
+
+    function percentageFraction() external pure returns (uint256);
+
+    function taxEnabledDuration() external pure returns (uint256);
+}
